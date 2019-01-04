46e7d974b41a7c6626233b5390cfc37012d60c64
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-01-04 02:56:41 +0200

    Add files via upload

---
+++ 1.1 class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_1._1._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random num = new Random();
+
+            for (int i = 1; i < 100; i++)
+            {
+
+                int y = 2;
+                int x = num.Next(1, 100);
+                while (x % y != 0 && y <= x)
+                    y++;
+                if (y < x || x == 1)
+                {
+                    continue;
+                }
+                else
+                    
+                Console.WriteLine(x);
+                break;
+            }
+          
+        }
+    }
+}
