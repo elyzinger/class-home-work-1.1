66f68004531a25a3708f07077a1208381e6bf934
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-01-04 04:47:22 +0200

    Add files via upload

---
+++ piramide.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please choose a num from 1-10");
+            int size = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 1; i <= size; i++)
+                
+            {
+               /* for (int l = 1; l <= i; l++)
+                {
+
+                    Console.Write(l);
+                }
+                for (int l = i + 1; l <= size; l++)
+                {
+                    Console.Write("*");
+                }
+                */
+                for(int j = 1; j<=size; j++)
+                {
+                    
+                    if(j<=i)
+                    {
+                        Console.Write(j);
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+
+                    
+                }
+            }
+        }
+    }
+}
